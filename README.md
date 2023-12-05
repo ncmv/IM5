339824b110fd42f9da6269ed8527054f40fbdf3e
ncmv <nora.moussa@stud.fhgr.ch>
2023-12-05 11:57:21 +0100

    Repository Move
    Da ich mich während der Projektwoche aber nach dem GitHub Setup für ein anderes Projekt entschieden habe, hatte ich einen neuen Ordner für das neue Projekt erstellt und vergessen das zu Beginn erstellte Repository neu zu verknüpfen. Jetzt habe ich keinen anderen Ausweg mehr gefunden als alle Dateien aus meinem neuen und finalen Projekt in den Ordner des alten zu kopieren, um mit GitHub arbeiten zu können.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# IM5
- Semesterprojekt
+# IM5 Semesterprojekt
+
+## Einleitung
+
+Der Arbeitstitel meiner Webseite ist 35nm, dies bezieht sich auf meine Initialen und die 35mm Fotos, die ich als Platzhalter verwendet habe. 
+
+Die Webseite habe ich allerdings in Gedanken an mein Lehrprojekt erstellt. Sie soll Platz für verschiedene Erkenntnisse aus meiner Arbeit bieten, die mit Bildern und Text erklärt werden sollen. Da ich mich in meiner Bachelorarbeit mit visuellen Stilmitteln in Video-Kampagnen beschäftige, könnte ich auf dieser Webseite die analysierten Stilmittel aufzeigen und erklären.
+
+Die Links im Footer führen im Moment alle noch auf die Homepage, sollen aber später auf das Digezz und Instagram meiner Bachelorarbeit führen. Sowie meine Kontaktangaben enthalten.
+
+Auf der About-Seite soll man zukünftig eine Erklärung zu meinem Lehrprojekt, sowie Infos zu meiner Person finden.
+
+Dies ist alles noch provisorisch, da ich mein Bachelor-Thema kürzlich geändert habe und daher die Seite noch nicht mit sinnvollem und finalem Inhalt befüllen kann. Dies ist auch der Grund für die noch repetitiven Bilder innerhalb der Galerien.
+
+## Prozess
+
+**Lightboxen**
+Speziell an meiner Webseite ist, dass alle Bilder sowie die About-Seite mit Lightboxen erstellt wurden. Deren Funktionalität und Besonderheiten werde ich im Video erläutern.
+Zuvor hatte ich jedoch noch nie mit Lightboxen zu tun. Für mein Vorhaben haben sie sich allerdings sehr gut geeignet und ich bin froh sie kennengelernt zu haben. 
+
+**Mobile Responsiveness**
+Als besondere Herausforderung stellte sich die Mobile-Version meiner Webseite heraus. Da sich die Darstellung und Funktionen innerhalb der Lightboxen, sowie die Aufteilung und Grösse der Bilder auf der Homepage, auf Mobile komplett ändern, steckt ein grosser Aufwand hinter den Anpassungen. In diesen Anpassungen hatte ich am meisten mit Bugs zu kämpfen. 
+
+**UI/UX**
+Es war mir wichtig, meiner Webseite ein schlichtes Design zu geben, damit vor allem die Bilder ins Auge springen. Dafür habe ich einen grossen Fokus auf Details gelegt. Beispielsweise die Animationen beim Öffnen und Schliessen der Lightboxen, das smoothe Scrollverhalten, den Back-To-Top-Button und die Scroll-Top Funktion innerhalb der Galerien. Diese sollen eine angenehme Userexperience ermöglichen.
+Um aber auch technisch sinnvoll vorzugehen, habe ich beispielsweise beim Text in den Galerien einen Vertical-Scroll ermöglicht, sollte der Text einmal länger werden, als der maximal sichtbare Bereich. Zudem habe ich auch wer darauf gelegt, die Bilder so gross wie möglich zur Verfügung zu stellen. Beispielsweise durch die Hover-Vergrösserung auf der Homepage und die 50 % Ausnützung der jeweiligen Bildschirmgrösse innerhalb der Galerien.
+
+**Learnings**
+Generell habe ich beim Erstellen dieser Seite sehr viel gelernt. Von Beginn an mit HTML, CSS und JavaScript zu arbeiten und die Seiteninhalte zu verknüpfen hat mein, in IM bisher gelerntes Wissen vertieft. Zudem hat es mir geholfen, in allen Files mit Kommentaren zu arbeiten. So verstand ich den Code auf Anhieb wieder, auch nach einigen Tagen, an denen ich nicht an diesem Projekt gearbeitet habe.
+
+**Vorgenen**
+Beim Erstellen meiner Webseite habe ich vor allem mit ChatGPT gearbeitet. Dabei habe ich die Prompts so genau wie möglich formuliert. ChatGPT war sehr hilfreich, jedoch stellen inhaltliche Zusammenhänge eine Hürde dar. Zum Beispiel habe ich GPT nach einer Funktion für die Gallerie-Lightboxen gefragt. Obwohl ich eine korrekte Funktion erhalten habe, hat ChatGPT nicht automatisch realisiert, dass ich die gleiche Funktion auch in den Mobile-Funktionen integrieren muss, obwohl GPT den genauen Aufbau meines js-Files kannte. Diese Hürden finde ich jedoch nicht schlimm, oder sogar gut. Denn so habe ich auch immer mitgedacht und ein Verständnis für den Code mitentwickelt. Beim Bugfixing war es ebenfalls hilfreich, kritisch nachzufragen. Beispielsweise habe ich Prompt geschrieben wie: "Könnte der Fehler nicht daraus entstehen, dass der Text sich innerhalb eines Content-divs befindet?". Das Mitdenken und Hinterfragen der Resultate von ChatGPT beschleunigte das Finden von meinen gesuchten Antworten sehr.
+
+**Quellen**
+Hilfe beim Coden: ChatGPT
+Einzelne Fragen: Google 
+Font: Google Fonts
+Bilder: Eigene Fotos
+
+## Schlusswort
+
+Alles in allem bin ich mit meiner Webseite sehr zufrieden, da das Resultat meinen Vorstellungen entspricht. Es hat mir Freude bereitet an einer Seite zu arbeiten, die mir gefällt und die ich zukünftig brauchen kann. So wurde jede funktionierende Zeile Code zu einem Erfolgserlebnis.
--- script.js
+++ script.js
@@ -1,25 +1,138 @@
-// VARIABLES
-const elH = document.querySelectorAll(".timeline li > div");
+//LIGHTBOX ÖFFNEN
+function openLightbox() {
+  document.getElementById('lightbox').style.display = 'flex';
+  document.body.style.overflow = 'hidden'; // Verhindert das Scrollen
 
-// START
-window.addEventListener("load", init);
-
-function init() {
-  setEqualHeights(elH);
-}
-
-// Gleiche Höhen der Boxen
-function setEqualHeights(el) {
-  let counter = 0;
-  for (let i = 0; i < el.length; i++) {
-    const singleHeight = el[i].offsetHeight;
-
-    if (counter < singleHeight) {
-      counter = singleHeight;
+  // ZURÜCKSETZTEN SCROLL-POSITION
+  var lightboxScrollContainer = document.querySelector('.lightbox-scroll');
+    if (lightboxScrollContainer) {
+        lightboxScrollContainer.scrollTop = 0;
     }
+}
+
+//GALLERIE LIGHTBOX SCHLIESSEN
+function closeLightbox() {
+  var lightbox = document.getElementById('lightbox');
+  lightbox.classList.add('closing');
+
+  // Warten auf das Ende der Animation, bevor die Lightbox ausgeblendet wird
+  lightbox.addEventListener('animationend', function() {
+    lightbox.style.display = 'none';
+    lightbox.classList.remove('closing');
+    document.body.style.overflow = 'auto'; // Setzt Scrollen auf Standard zurück
+  }, { once: true });
+}
+
+//LIGHTBOX-SCROLL-BILDER RICHTIG ANZEIGEN ANHAND GALLERY AUSGANGSBILD
+function setCurrentImage(clickedImg) {
+  var lightboxImages = clickedImg.getAttribute('data-lightbox-images').split(',');
+  var lightboxText = clickedImg.getAttribute('data-lightbox-text');
+  var lightboxDetailText = clickedImg.getAttribute('data-lightbox-detail-text');
+
+  var lightboxGallery = document.querySelector('.lightbox-scroll');
+  lightboxGallery.innerHTML = ''; // Entfernt alle aktuellen Bilder
+
+  lightboxImages.forEach(imgSrc => {
+    var imgElement = document.createElement('img');
+    imgElement.src = imgSrc;
+    imgElement.classList.add('lightbox-image');
+    lightboxGallery.appendChild(imgElement);
+  });
+
+  var lightboxTextElement = document.querySelector('.lightbox-text');
+  lightboxTextContainer.innerHTML = ''; // Leere den vorhandenen Text
+
+  // Fügt den Titeltext hinzu
+  var titleElement = document.createElement('p');
+  titleElement.textContent = lightboxText;
+  lightboxTextContainer.appendChild(titleElement);
+
+ // Prüft, ob der Fließtext bereits existiert, und aktualisiert oder erstellt ihn
+ var existingDetailElement = lightboxTextContainer.querySelector('.lightbox-detail-text');
+ if (existingDetailElement) {
+     existingDetailElement.textContent = lightboxDetailText;
+ } else {
+     var detailElement = document.createElement('p');
+     detailElement.textContent = lightboxDetailText;
+     detailElement.classList.add('lightbox-detail-text');
+     lightboxTextContainer.appendChild(detailElement);
+ }
+}
+
+//ABOUT LIGHTBOX FUNKTIONEN
+function openAbout() {
+  document.getElementById('aboutBox').style.display = 'flex';
+  document.body.style.overflow = 'hidden'; // Verhindert das Scrollen
+}
+
+function closeAbout() {
+  var aboutBox = document.getElementById('aboutBox');
+  aboutBox.classList.add('closing');
+
+  aboutBox.addEventListener('animationend', function() {
+      aboutBox.style.display = 'none';
+      aboutBox.classList.remove('closing');
+      document.body.style.overflow = 'auto'; // Setzt Scrollen auf Standard zurück
+  }, { once: true });
+}
+
+//BACK-TO-TOP-BUTTON
+function topFunction() {
+  document.body.scrollTop = 0; // Für Safari
+  document.documentElement.scrollTop = 0; // Für Chrome, Firefox, IE und Opera
+}
+
+// Back to Top Button sichtbar machen, sobald gescrollt wird
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.querySelector('.back-to-top').style.display = 'block';
+  } else {
+      document.querySelector('.back-to-top').style.display = 'none';
+  }
+};
+
+// AUF MOBILE LIGHTBOX-GALLERY AUF 1. BILD BESCHRÄNKEN
+function setCurrentImage(clickedImg) {
+  var lightboxImages = clickedImg.getAttribute('data-lightbox-images').split(',');
+  var lightboxText = clickedImg.getAttribute('data-lightbox-text');
+  var lightboxDetailText = clickedImg.getAttribute('data-lightbox-detail-text');
+
+  var lightboxGallery = document.querySelector('.lightbox-scroll');
+  lightboxGallery.innerHTML = ''; // Entfernt alle aktuellen Bilder
+
+  // Überprüfen, ob die Seite auf einem mobilen Gerät angezeigt wird
+  if (window.innerWidth <= 768) {
+      // Nur das erste Bild für mobile Geräte hinzufügen
+      var imgElement = document.createElement('img');
+      imgElement.src = lightboxImages[0];
+      imgElement.classList.add('lightbox-image');
+      lightboxGallery.appendChild(imgElement);
+  } else {
+      // Alle Bilder für Desktop hinzufügen
+      lightboxImages.forEach(imgSrc => {
+          var imgElement = document.createElement('img');
+          imgElement.src = imgSrc;
+          imgElement.classList.add('lightbox-image');
+          lightboxGallery.appendChild(imgElement);
+      });
   }
 
-  for (let i = 0; i < el.length; i++) {
-    el[i].style.height = `${counter}px`;
+  var lightboxTextContainer = document.querySelector('.lightbox-text');
+  lightboxTextContainer.innerHTML = ''; // Leert den vorhandenen Text
+
+  var titleElement = document.createElement('p');
+  titleElement.textContent = lightboxText;
+  lightboxTextContainer.appendChild(titleElement);
+
+  // Prüft, ob der Fließtext bereits existiert, und aktualisiert oder erstellt ihn
+  var existingDetailElement = lightboxTextContainer.querySelector('.lightbox-detail-text');
+  if (existingDetailElement) {
+      existingDetailElement.textContent = lightboxDetailText;
+  } else {
+      var detailElement = document.createElement('p');
+      detailElement.textContent = lightboxDetailText;
+      detailElement.classList.add('lightbox-detail-text');
+      lightboxTextContainer.appendChild(detailElement);
   }
 }
+
